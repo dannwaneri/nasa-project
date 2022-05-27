f38ab5a4add2da6f615bc1ff627f84171964720f
Penuelnwaneri <penuelchudi@gmail.com>
2022-05-27 05:50:18 +0100

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,12 @@ app.use(cors({
   }));
 
 app.use(express.json());
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
   
 app.use(planetsRouter);
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-  });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 
 module.exports = app; 
