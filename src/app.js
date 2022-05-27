11e0356d7823a8b48ba5bbb66432d1b281d3191c
Penuelnwaneri <penuelchudi@gmail.com>
2022-05-27 13:15:56 +0100

    fixed error

--- src/app.js
+++ src/app.js
@@ -10,11 +10,11 @@ app.use(cors({
   }));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, './client/build')))
   
 app.use(planetsRouter);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
 });
 
 
