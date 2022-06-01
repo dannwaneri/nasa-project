944845e26d3907f896462951053f2a5fa4126bee
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-01 15:16:45 +0100

    updated

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.use(cors({
   }));
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')))
   
 app.use(planetsRouter);
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
   })
 
 
