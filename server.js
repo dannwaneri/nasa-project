11be5310acf3c22f9f32ada43af2141eaede266d
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-01 15:04:32 +0100

    error fixed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http')
 const { loadPlanetsData } = require('./models/planets.model');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
