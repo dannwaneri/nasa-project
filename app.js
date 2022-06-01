f80cb1876f0a6aee829f68c233326822435e1a76
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-01 14:13:29 +0100

    error fixed

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.use(cors({
   }));
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, '../client/build')))
   
 app.use(planetsRouter);
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  })
 
 
 module.exports = app; 
--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:3001';
 
 
 
