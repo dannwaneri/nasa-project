36d5edf8ed8b3479026a2a3f4d0c153fdce372e0
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-04 07:33:48 +0100

    API tests with supertestt

--- routes/launches/launches.test.js
+++ routes/launches/launches.test.js
@@ -1,20 +1,75 @@
+const request = require('supertest');
+const app = require('../../app')
+
 // We can create a test fixtures with different test cases by using this describe function.
 // And passing in  a description for our group of tests.
 // our tests are defined in this callback that we pass into the describe function which can call this test function 
 // that defines each of our test cases.
 
 describe('Test GET /launches', () => {
-    test('It should respond with 200 success', () => {
-      const response = 200;
-      expect(response).toBe(200);
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
     });
   });
 
 
   describe('Test POST /launches', () => {
-      test('It should respond with 200 success', () => {
 
+    const completeLaunchData = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028',
+      };
+    
+      const launchDataWithoutDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+      };
+
+      const launchDataWithInvalidDate = {
+        mission: 'USS Enterprise',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'zoot',
+      };
+
+      test('It should respond with 201 created', async () => {
+        const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+        .expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+       const responseDate = new Date(response.body.launchDate).valueOf();
+       expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
       })
-      test('It should catch missing required properties', () => {});
-  test('It should catch invalid dates', () => {});
+      test('It should catch missing required properties', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(launchDataWithoutDate)
+        .expect('Content-Type', /json/)
+        .expect(400);
+        expect(response.body).toStrictEqual({
+            error: 'Missing required launch property',
+          });
+      });
+      test('It should catch invalid dates', async () => {
+        const response = await request(app)
+          .post('/launches')
+          .send(launchDataWithInvalidDate)
+          .expect('Content-Type', /json/)
+          .expect(400);
+    
+        expect(response.body).toStrictEqual({
+          error: 'Invalid launch date',
+        });
+      });
   })
