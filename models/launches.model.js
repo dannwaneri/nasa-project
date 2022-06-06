8db2946ddf22abee8aa02ccc9c5ac0725ab4912c
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-06 10:29:25 +0100

    Verify target planet existst

--- models/launches.model.js
+++ models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -22,6 +23,12 @@ async function getAllLaunches() {
 }
 
   async function saveLaunch(launch) {
+    const planet = await planets.findOne({
+      keplerName: launch.target,
+    })
+    if (!planet){
+      throw new Error('No matching planet found');
+    }
     await launchesDatabase.updateOne({
       flightNumber: launch.flightNumber,
     }, launch, {
