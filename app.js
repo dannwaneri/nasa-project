c2af5a5ec60c8bcc6e75ac8a59b792f4eb4a7b01
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-07 15:14:12 +0100

    Add versioning to APIt

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
 const app = express();
+
+const api = require('./routes/api');
+
 
 app.use(cors({
     origin: ['http://localhost:3000/', 'https://mysterious-depths-27411.herokuapp.com/']
@@ -13,8 +14,7 @@ app.use(express.json());
 app.use(express.static(path.resolve(__dirname, './client/build')))
 //app.use(express.static(path.join(__dirname, '.', 'public')));
   
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+app.use('/v1', api);
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
  // res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/v1',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'http://localhost:3001/v1',
       changeOrigin: true,
     })
   );
