a9ebd82121c07c804997376367d0c88b630cca43
Penuelnwaneri <penuelchudi@gmail.com>
2022-06-09 11:35:42 +0100

    Populate launches if not already loadedt

--- models/launches.model.js
+++ models/launches.model.js
@@ -22,13 +22,15 @@ const launch = {
 
 saveLaunch(launch)
 
+
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
-async function loadLaunchData(){
+async function populateLaunches(){
   console.log(`Downloading launch data .......`);
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
+      pagination: false,
       populate: [
         {
           path: 'rocket',
@@ -45,28 +47,44 @@ async function loadLaunchData(){
       ]
     }
   });
-
   const launchDocs = response.data.docs;
-for(const launchDoc of launchDocs){
-  const payloads = launchDoc['payloads'];
-  const customers = payloads.flatMap((payload) => {
-    return payload['customers'];
-  });
-  const launch = {
-    flightNumber: launchDoc['flight_number'],
-    mission: launchDoc['name'],
-    rocket: launchDoc['rocket']['name'],
-    launchDate: launchDoc['date_local'],
-    upcoming: launchDoc['upcoming'],
-    success: launchDoc['success'],
-    customers,
+  for(const launchDoc of launchDocs){
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers'];
+    });
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    }
+  
+    console.log(`${launch.flightNumber} ${launch.mission}`);
   }
-
-  console.log(`${launch.flightNumber} ${launch.mission}`);
-}
 }
 
 
+async function loadLaunchData(){
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: 'Falcon 1',
+    mission: 'FalconSat',
+  });
+  if (firstLaunch) {
+    console.log('Launch data already loaded!');
+  } else {
+    await populateLaunches();
+  }
+}
+
+
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
 
 
 
@@ -115,7 +133,7 @@ async function getAllLaunches() {
 
 
 async function existsLaunchWithId(launchId) {
-  return await launchesDatabase.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   });
   }
